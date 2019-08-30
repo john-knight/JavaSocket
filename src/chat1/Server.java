d0a532246d01c50730b416ef940fad9cc43bd234
Administrator <2962719743@qq.com>
2019-08-30 15:35:49 +0800

    一个用socket编写的建议的聊天室

--- src/chat1/Server.java
+++ src/chat1/Server.java
@@ -8,9 +8,9 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args)throws IOException {
-       ServerSocket serverSocket=new ServerSocket(9999);
+        ServerSocket serverSocket=new ServerSocket(9999);
 //       用来接收客户端发送的socket请求
-       Socket socket=serverSocket.accept();
+        Socket socket=serverSocket.accept();
         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
         DataInputStream in = new DataInputStream(socket.getInputStream());
         String string =in.readUTF();
